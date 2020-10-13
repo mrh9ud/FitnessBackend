b893d36d0960bc101fb02468a39316b4c044d1af
mrh9ud <mrh9ud@virginia.edu>
2020-10-13 17:03:12 -0400

    Revert "first connection with backend established from front"
    This reverts commit caf97f3f0977c220674461b12fd1499c743dd882.

---
+++ FitnessBackend/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user) }, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
+    end
+end
