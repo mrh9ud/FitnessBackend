3697b0457dc90667c4c359683a67fa03814882cb
mrh9ud <mrh9ud@virginia.edu>
2020-10-14 17:44:16 -0400

    backend token authentication for login is functional

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,23 @@ class Api::V1::AuthController < ApplicationController
     def create
         user = User.find_by(username: params[:username])
         if user
-            render json: user
+            if user.authenticate(params[:password])
+                token = encode(user_id: user.id)
+                render json: {
+                    user: user,
+                    jwt: token
+                }, status: :accepted
+            else
+                render json: {
+                    error: true,
+                    message: "Incorrect Password"
+                }, status: :unauthorized
+            end
+        else
+            render json: {
+                error: true,
+                message: "Invalid Username"
+            }, status: :bad_request
         end
     end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,6 +15,6 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password, :first_name, :last_name, :email)
+        params.require(:user).permit(:id, :username, :password, :first_name, :last_name, :email)
     end
 end
