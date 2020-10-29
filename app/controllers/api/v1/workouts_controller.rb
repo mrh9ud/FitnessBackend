d8eeb244fc29210f1794a2313f72f9196373d603
rhode2nh <rhode2nh@protonmail.com>
2020-10-29 13:59:13 -0400

    added instructions to seed data

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::WorkoutsController < ApplicationController
     def create
-        byebug
+        workout = Workout.create(workout_params)
+        user_workout = UserWorkout.create({ "user_id" => params[:id], "workout_id" => workout[:id], "completed" => false})
+        render json: workout.as_json(only: [:id, :strength, :cardio, :difficulty, :duration]), status: :accepted
     end
 
     private
