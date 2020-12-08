85f21021c33076b692186cc0bffd91625d08f87f
mrh9ud <mrh9ud@virginia.edu>
2020-12-07 22:18:46 -0500

    progress on algorithm for user workout creation

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -12,7 +12,23 @@ class Api::V1::ExercisesController < ApplicationController
   end
 
   def find_queried_exercises
-    byebug
+    filtered_by_name = Exercise.exercises_by_name(params[:search_query])
+    if filtered_by_name.empty?
+      render json: { exercises: [] }
+    else
+      filtered_by_focus = Exercise.exercises_by_focus(filtered_by_name, params[:focus])
+      if filtered_by_focus.empty?
+        render json: { exercises: [] }
+      else
+        filtered_by_difficulty = filtered_by_focus.where(difficulty: params[:difficulty])
+        if filtered_by_difficulty.empty?
+          render json: { exercises: [] }
+        else
+          fully_filtered = filtered_by_difficulty.exercises_by_muscle_groups(filtered_by_difficulty, params[:muscle_groups])
+        end
+      end
+    end
+
   end
 
 end
