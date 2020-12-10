b789f4d4c3debdd8ff651f0601f98c5933e863e1
rhode2nh <rhode2nh@protonmail.com>
2020-12-10 15:16:49 -0500

    fixed primary/secondary muscles query method

--- app/controllers/api/v1/exercises_controller.rb
+++ app/controllers/api/v1/exercises_controller.rb
@@ -4,20 +4,27 @@ class Api::V1::ExercisesController < ApplicationController
     filtered_by_focus = exercise_params[:focus].empty? ? filtered_by_name : Exercise.exercises_by_focus(filtered_by_name, exercise_params[:focus])
     filtered_by_difficulty = exercise_params[:difficulty] == 'all' ? filtered_by_focus : filtered_by_focus.where(difficulty: exercise_params[:difficulty])
     filtered_by_muscle_groups = filtered_by_difficulty.exercises_by_muscle_groups(filtered_by_difficulty, exercise_params[:muscle_groups])
-    exercise_id_primary_muscles = filtered_by_muscle_groups.includes(:exercise_muscle_groups, :muscle_groups)
-                                                           .pluck("exercise_muscle_groups.exercise_id", "muscle_groups.name")
-    filtered_exercises_hash = {}
-    fully_filtered_exercises = []
-    exercise_id_primary_muscles.each do |exercise_id_primary_muscle|
-      if filtered_exercises_hash.key?(exercise_id_primary_muscle[0])
-        filtered_exercises_hash[exercise_id_primary_muscle[0]] = filtered_exercises_hash[exercise_id_primary_muscle[0]] << exercise_id_primary_muscle[1]
-      else
-        filtered_exercises_hash[exercise_id_primary_muscle[0]] = [exercise_id_primary_muscle[1]]
-      end
+
+    filtered_exercise_ids = filtered_by_muscle_groups.pluck(:id)
+    filtered_exercises = Exercise.find(filtered_exercise_ids)
+    exercise_muscle_groups = []
+
+    filtered_exercises.each do |filtered_exercise|
+      exercise_muscle_groups << filtered_exercise.exercise_muscle_groups
     end
-    filtered_exercises_hash.each do |key, value|
+
+    primary_muscles_hash = {}
+    secondary_muscles_hash = {}
+    exercise_muscle_groups.each do |exercise_muscle_group|
+      primary_muscles_hash[exercise_muscle_group.pluck(:exercise_id).first] = exercise_muscle_group.where(primary: true).pluck(:muscle_group_id)
+      secondary_muscles_hash[exercise_muscle_group.pluck(:exercise_id).first] = exercise_muscle_group.where(primary: false).pluck(:muscle_group_id)
+    end
+
+    fully_filtered_exercises = []
+    primary_muscles_hash.each do |key, value|
       exercise = Exercise.find(key).as_json
-      exercise[:primary] = value
+      exercise[:primary] = MuscleGroup.find(value).pluck(:name)
+      exercise[:secondary] = MuscleGroup.find(secondary_muscles_hash[key]).pluck(:name)
       fully_filtered_exercises << exercise
     end
     render json: { exercises: fully_filtered_exercises }
