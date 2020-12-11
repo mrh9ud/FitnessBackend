f24f26714b49b978d97abf9e96b978e7706de9a3
rhode2nh <rhode2nh@protonmail.com>
2020-12-11 11:35:58 -0500

    abstracted methods

--- app/models/exercise_muscle.rb
+++ app/models/exercise_muscle.rb
@@ -1,4 +1,34 @@
 class ExerciseMuscle < ApplicationRecord
     belongs_to :exercise
     belongs_to :muscle
+
+    def self.get_exercise_muscle_ids(exercises_arr)
+        exercise_muscles = []
+
+        exercises_arr.each do |exercise|
+            exercise_muscles << exercise.exercise_muscles
+        end
+        exercise_muscles
+    end
+
+
+    # @return [Hash] key of exercise_ids and a value of an array of primary muscle_ids
+    def self.get_exercise_primary_muscles(exercise_muscles)
+        primary_muscles_hash = {}
+
+        exercise_muscles.each do |exercise_muscle|
+            primary_muscles_hash[exercise_muscle.pluck(:exercise_id).first] = exercise_muscle.where(primary: true).pluck(:muscle_id)
+        end
+        primary_muscles_hash
+    end
+
+    # @return [Hash] key of exercise_ids and a value of an array of secondary muscle_ids
+    def self.get_exercise_secondary_muscles(exercise_muscles)
+        secondary_muscles_hash = {}
+
+        exercise_muscles.each do |exercise_muscle|
+            secondary_muscles_hash[exercise_muscle.pluck(:exercise_id).first] = exercise_muscle.where(primary: false).pluck(:muscle_id)
+        end
+        secondary_muscles_hash
+    end
 end
--- app/models/exercise_muscle_group.rb
+++ app/models/exercise_muscle_group.rb
@@ -1,4 +1,34 @@
 class ExerciseMuscleGroup < ApplicationRecord
   belongs_to :exercise
   belongs_to :muscle_group
+
+  def self.get_exercise_muscle_group_ids(exercises_arr)
+    exercise_muscle_groups = []
+
+    exercises_arr.each do |exercise|
+      exercise_muscle_groups << exercise.exercise_muscle_groups
+    end
+
+    exercise_muscle_groups
+  end
+
+  # @return [Hash] key of exercise_ids and a value of an array of primary muscle_group_ids
+  def self.get_exercise_primary_muscle_groups(exercise_muscle_groups)
+    primary_muscle_groups_hash = {}
+
+    exercise_muscle_groups.each do |exercise_muscle_group|
+      primary_muscle_groups_hash[exercise_muscle_group.pluck(:exercise_id).first] = exercise_muscle_group.where(primary: true).pluck(:muscle_group_id)
+    end
+    primary_muscle_groups_hash
+  end
+
+  # @return [Hash] key of exercise_ids and a value of an array of secondary muscle_group_ids
+  def self.get_exercise_secondary_muscle_groups(exercise_muscle_groups)
+    secondary_muscle_groups_hash = {}
+
+    exercise_muscle_groups.each do |exercise_muscle_group|
+      secondary_muscle_groups_hash[exercise_muscle_group.pluck(:exercise_id).first] = exercise_muscle_group.where(primary: false).pluck(:muscle_group_id)
+    end
+    secondary_muscle_groups_hash
+  end
 end
